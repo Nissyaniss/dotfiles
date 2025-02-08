464858a3b26f0a5ef032f0ab869a33f7606f4ae7
Nissya <69462618+Nissyaniss@users.noreply.github.com>
2025-02-08 17:26:03 +0100

    Update wezterm.lua for windows yet again

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.window_background_opacity = 0.5 -- 0.7 for windows
+-- config.default_prog = { 'C:\\Users\\test\\scoop\\shims\\nu.exe' }
 config.font = wezterm.font('Fira Code Nerd Font', { weight = 'Regular' })
 
 local act = wezterm.action
